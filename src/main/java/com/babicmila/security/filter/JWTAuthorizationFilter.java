277f46ffd43dd690edd69e7bcdbb1be223f62dd1
babicmila <milababicwork@gmail.com>
2024-07-26 19:37:58 +0200

    added environment variables

--- src/main/java/com/babicmila/security/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/babicmila/security/filter/JWTAuthorizationFilter.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.auth0.jwt.JWT;
